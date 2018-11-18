9d98d8c358312d2e789802ab7ba9573882dc6415
Peter Kristensen <peter@zmartify.dk>
2018-11-18 22:41:50 +0100

    1.3.0 first production release

--- src/app/app.version.ts
+++ src/app/app.version.ts
@@ -1,2 +1,2 @@
-// This file was generated on Sun Nov 18 07:41:53 CET 2018
-export const appVersion = '1.2.9';
+// This file was generated on Sun Nov 18 22:26:07 CET 2018
+export const appVersion = '1.3.0';
