55770bd5857209935ef8288e5d26297b1847e0db
Peter Kristensen <peter@zmartify.dk>
2018-11-18 22:57:47 +0100

    1.4.0 first production release

--- src/app/app.version.ts
+++ src/app/app.version.ts
@@ -1,2 +1,2 @@
-// This file was generated on Sun Nov 18 22:26:07 CET 2018
-export const appVersion = '1.3.0';
+// This file was generated on Sun Nov 18 22:53:48 CET 2018
+export const appVersion = '1.4.0';
