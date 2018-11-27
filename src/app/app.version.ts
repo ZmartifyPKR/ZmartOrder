2600377309bb7ae0bd464cc60f91925cdf16ea24
Peter Kristensen <peter@zmartify.dk>
2018-11-27 19:42:39 +0100

    1.4.3.

--- src/app/app.version.ts
+++ src/app/app.version.ts
@@ -1,2 +1,2 @@
-// This file was generated on Tue Nov 27 18:51:30 CET 2018
-export const appVersion = '1.4.2';
+// This file was generated on Tue Nov 27 19:42:25 CET 2018
+export const appVersion = '1.4.3';
