3bb107cd08fba51bad484aff007f35a4e742d473
Peter Kristensen <peter@zmartify.dk>
2018-11-28 21:40:12 +0100

    1.4.5 solved upload error on ios

--- src/app/app.version.ts
+++ src/app/app.version.ts
@@ -1,2 +1,2 @@
-// This file was generated on Tue Nov 27 20:04:17 CET 2018
-export const appVersion = '1.4.4';
+// This file was generated on Wed Nov 28 21:39:58 CET 2018
+export const appVersion = '1.4.5';
