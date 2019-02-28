a4a0c46f4b3747e34c90efbd3112c7fb9ada3d99
Peter Kristensen <peter@zmartify.dk>
2019-02-28 16:02:36 +0100

    Polish language and jumped to version 1.4.7

--- src/app/app.version.ts
+++ src/app/app.version.ts
@@ -1,2 +1,2 @@
-// This file was generated on Wed Dec  5 21:22:46 CET 2018
-export const appVersion = '1.4.6';
+// This file was generated on Thu Feb 28 16:02:11 CET 2019
+export const appVersion = '1.4.7';
